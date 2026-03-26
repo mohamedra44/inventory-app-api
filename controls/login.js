b928f6eaea6877bd5371adc419668636e153aa7a
MohamedRa44 <mohamedrabiema44@gmail.com>
2026-03-26 18:06:57 +0200

    fixed error

--- controls/login.js
+++ controls/login.js
@@ -2,18 +2,17 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-
 module.exports = async (req, res, next) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) return res.status(404).json({ message: "User not found" });
 
   const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(400).json({ message: "Invalid email or password" });
-
+  if (!isMatch)
+    return res.status(400).json({ message: "Invalid email or password" });
 
   const token = jwt.sign(
-    { id: user._id, email: user.email, role: user.role },
+    { id: user._id, name: user.name, email: user.email, role: user.role },
     process.env.JWT_SECRET,
     { expiresIn: "1d" },
   );
